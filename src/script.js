9eacd3f6b8c56e9ab10e8d3f880e3bc6f1849769
williampol <103951515+williampol@users.noreply.github.com>
2022-07-04 22:24:35 -0400

    Implementing buttons
    The buttons now work. When pressed, the increment the value in the textbox by 1. Next is implementation is the countdown.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,52 @@
 "use strict";
+
+window.onload = function() {
+    
+    const incHour = document.getElementById("hour-inc");
+    const decHour = document.getElementById("hour-dec");
+    const incMin = document.getElementById("min-inc");
+    const decMin = document.getElementById("min-dec");
+    const incSec = document.getElementById("sec-inc");
+    const decSec = document.getElementById("sec-dec");
+
+    const hourBox = document.getElementById("hours");
+    const minBox = document.getElementById("minutes");
+    const secBox = document.getElementById("seconds");
+
+    incHour.addEventListener("click", () => {
+        incOne(hourBox);
+    });
+
+    decHour.addEventListener("click", () => {
+        decOne(hourBox);
+    });
+
+    incMin.addEventListener("click", () => {
+        incOne(minBox);
+    });
+
+    decMin.addEventListener("click", () => {
+        decOne(minBox);
+    });
+
+    incSec.addEventListener("click", () => {
+        incOne(secBox);
+    });
+
+    decSec.addEventListener("click", () => {
+        decOne(secBox);
+    });
+
+}
+
+function incOne(text) {
+    let current = text.getAttribute("value");
+    text.setAttribute("value", ("0" + (+current + 1)).slice(-2));
+}
+
+function decOne(text) {
+    let current = text.getAttribute("value");
+    if (+current) {
+        text.setAttribute("value", ("0" + (+current - 1)).slice(-2));
+    }
+}
